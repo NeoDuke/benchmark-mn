1f0dc8b38e142a72cd3972236a9abf78f31954c0
Borja Rosco Alcina <neo_duke@hotmail.com>
2020-03-03 20:48:42 +0100

    rename project

--- helloworld-micronaut-native/docker-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-SECONDS=0
-
-docker build -t neoduke/helloworld-micronaut-native:1.0.0 .
-
-duration=$SECONDS
-echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
